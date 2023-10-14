6908f10a1f5848706eea74f7dcb968c720ca13cc
Aleksandar Valchev <aleksandar.valchev@sap.com>
2023-10-14 11:12:36 +0300

    .

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,6 +5,9 @@ import session from 'express-session';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import { Strategy as GoogleStrategy } from 'passport-google-oidc';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 const PORT = 3000;
@@ -26,15 +29,24 @@ app.use(
   })
 );
 
-const gStrategy = new GoogleStrategy({ clientID: 'x', clientSecret: 'x', callbackURL: 'x' }, 
+passport.use(new GoogleStrategy(
+  { 
+    clientID: process.env['GOOGLE_CLIENT_ID'] as string,
+    clientSecret: process.env['GOOGLE_CLIENT_SECRET'] as string,
+    callbackURL: process.env['GOOGLE_CALLBACK'] as string
+  }, 
 function verify(issuer: string, profile: { id: string; displayName: string }, cb: (error?: Error, user?: object | false) => void) {
 
-});
-
-passport.use(gStrategy)
+}));
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
+});
+app.get('/auth/google/callback', passport.authenticate('oauth2', { scope: ["profile"], failureRedirect: '/login', successRedirect: '/' }));
+app.get('/auth/google', passport.authenticate('oauth2'));
+app.use((err: any, req: Request, res: Response, next: Function) => {
+  console.error(err.stack)
+  res.status(500).send('An error occurred')
 });
 
 app.listen(PORT, () => {
